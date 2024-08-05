de0cbc163ae05e9b28bc7be46db70245844632e6
RonaZhen <zhenheru1@gmail.com>
2024-08-05 11:10:57 +0800

    Update 0claude.js

--- scr/cmd/0claude.js
+++ scr/cmd/0claude.js
@@ -1,1 +1,32 @@
+module["exports"] = class {
+  static config = {
+    name: "claude",
+    description: "Talk to Claude AI",
+    prefix: false,
+    accessableby: 0,
+    author: "heru",
+  };
 
+  static async start({ reply, text, react, api, event }) {
+    const { get } = require("axios");
+    try {
+      let ask = text.join(" ");
+      if (!ask) return reply("Missing prompt!");
+      react("⏳");
+      const heru = await new Promise(resolve => {
+        api.sendMessage('Searching your question please wait...', event.threadID, (err, info) => {
+          resolve(info);
+        });
+      });
+
+      const rest = (
+        await get("https://hiroshi-rest-api.replit.app/ai/claude?ask=" + encodeURI(ask))
+      ).data;
+
+      react("✅");
+      await api.editMessage('🤖 | 𝙲𝚕𝚊𝚞𝚍𝚎 𝚁𝚎𝚜𝚙𝚘𝚗𝚜𝚎\n━━━━━━━━━━━━━━━━━━\n' + rest.result + "\n━━━━━━━━━━━━━━━━━━", heru.messageID);
+    } catch (e) {
+      return reply(e.message);
+    }
+  }
+};
